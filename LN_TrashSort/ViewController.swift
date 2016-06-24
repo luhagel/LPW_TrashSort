856083d5c9ac5709c527d24a125fe60dc439b1dd
Luca Hagel <hagel.luca@gmail.com>
2016-06-24 09:55:00 -0700

    Initial Commit

---
+++ LN_TrashSort/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LN_TrashSort
+//
+//  Created by Luca Hagel on 6/24/16.
+//  Copyright © 2016 Luca Hagel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
